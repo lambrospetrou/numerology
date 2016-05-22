baab6a59d4d31aae8029db5efabe399bc0770778
Lambros Petrou <lambrospetrou@gmail.com>
2016-05-22 21:37:42 +0100

    Added name calculator and some CSS.

--- src/scripts/components/BaseComponent.tsx
+++ src/scripts/components/BaseComponent.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 
-
 export class BaseComponent<P, S> extends React.Component<P, S> {
     // Helper to bind all methods to the 'this' of each component 
     _bind(...methods: string[]) {
         methods.forEach((method: string) => {
             this[method] = this[method].bind(this);
         });
+    }
+
+    _setState(partialState: any) {
+        this.setState(Object.assign({}, this.state, partialState));
     }
 }
--- src/scripts/components/Calculator.tsx
+++ src/scripts/components/Calculator.tsx
@@ -3,13 +3,14 @@ import { BaseComponent } from './BaseComponent';
 
 interface CalculatorProps {}
 interface CalculatorState {
-    inputBirth: string
+    inputBirth: string;
+    inputName: string;
 }
 
 export class Calculator extends BaseComponent<CalculatorProps, CalculatorState> {
     constructor() {
         super();
-        this._bind('_onChangeBirth', '_calculate');
+        this._bind('_onChangeBirth', '_onChangeName', '_calculateString');
         this.state = this._getInitialState();
     }
 
@@ -19,12 +20,17 @@ export class Calculator extends BaseComponent<CalculatorProps, CalculatorState> 
 
     private _getInitialState() {
         return {
-            inputBirth: Calculator.parseDate(new Date())
+            inputBirth: Calculator.parseDate(new Date()),
+            inputName: ''
         };
     }
 
     private _onChangeBirth(evt) {
-        this.setState({inputBirth: evt.target.value});
+        this._setState({inputBirth: evt.target.value});
+    }
+
+    private _onChangeName(evt) {
+        this._setState({inputName: evt.target.value});
     }
 
     private _calculateString(str: string) {
@@ -36,33 +42,48 @@ export class Calculator extends BaseComponent<CalculatorProps, CalculatorState> 
         }
 
         function sumCharacters(str: string) {
-            return Array.prototype.map.call(str, (ch) => {
+            return Array.prototype.map.call((str || '').toLowerCase(), (ch) => {
+                if (ch >= 'a' && ch <= 'z') {
+                    return Number(ch.charCodeAt(0) - 'a'.charCodeAt(0))+1;
+                }
                 return (ch >= '0' && ch <= '9') ? Number(ch) : 0;
             }).reduce((sum, current) => sum + current, 0);
         }
 
         function calculateCore(str: string) {
             const sumBirth: number = sumCharacters(str);
+            if (sumBirth == 0) {
+                return -1;
+            }
             return isCoreValue(sumBirth) ? sumBirth : calculateCore(String(sumBirth));
         }
 
         return calculateCore(str);
     }
 
-    private _calculate() {
-        return this._calculateString(this.state.inputBirth);
-    }
-
     render() {
         console.log(this.state);
         return (
             <div class="calculator-container">
-                <input type="date" name="input-birth" class="input-birth" 
-                    value={this.state.inputBirth}
-                    onChange={this._onChangeBirth}/>
-                <section class="numerology-result">
-                    <p>{this._calculate()}</p>
-                </section>
+                <div>
+                    <h3>Birth date</h3>
+                    <input type="date" name="input-birth" id="input-birth" 
+                        value={this.state.inputBirth}
+                        onChange={this._onChangeBirth}/>
+                    <section id="numerology-result-birth" className="numerology-result" >
+                        <p>{this._calculateString(this.state.inputBirth)}</p>
+                    </section>
+                </div>
+
+                <div>
+                    <h3>Name (A-Z only)</h3>
+                    <input type="text" name="input-name" id="input-name"
+                        value={this.state.inputName}
+                        onChange={this._onChangeName}/>
+                    <section id="numerology-result-name" className="numerology-result">
+                        <p>{this._calculateString(this.state.inputName)}</p>
+                    </section>
+                </div>
             </div>
         );
     }
